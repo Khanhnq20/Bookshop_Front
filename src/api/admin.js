3abfa16021e32fce812808403a3bdffbd139b3cf
OS <OS@DESKTOP-NLC0EVM>
2022-11-21 07:06:11 +0700

    get_staff_page

--- src/api/admin.js
+++ src/api/admin.js
@@ -9,3 +9,15 @@ const adminInstance = axios.create({
 export function getUser(){
     return adminInstance.get("getUser");
 }
+
+export function getStaff(){
+    return adminInstance.get("getStaff");
+}
+
+export function registerStaff(email, password,name,dayOfBirth,phoneNumber,gender){
+    return axios.post("api/accounts/register/staff",{email:email,password:password,name:name,dayOfBirth:dayOfBirth,phoneNumber:phoneNumber,gender:gender},{
+        baseURL: host,
+        withCredentials: 'same-site'
+    });
+} 
+
--- src/container/staffManagement/index.js
+++ src/container/staffManagement/index.js
@@ -1,10 +1,46 @@
 import React from 'react';
 import Component from '../../components/root';
+import Table from 'react-bootstrap/Table';
+import { getStaff } from '../../api/admin';
+import Button from 'react-bootstrap/Button';
 
 const StaffManagementContainer = () => {
+    const [staff,setStaff] = React.useState([]);
+    React.useEffect(()=>{
+        getStaff().then(res=>{
+            let data = res.data;
+            setStaff(data)
+        })
+    },[])
     return (
-        <Component>
-            
+        <Component className="user">
+            <Component style={{maxWidth:"800px",margin:"0 auto"}}>
+                <Component style={{marginBottom:"20px"}}>
+                    <Button variant="primary">Create New Staff</Button>
+                </Component>
+                <Table striped bordered hover>
+                    <thead>
+                        <tr>
+                        <th>#</th>
+                        <th>Staff Name</th>
+                        <th>Email</th>
+                        <th>Action</th>
+                        </tr>
+                    </thead>
+                    {staff?.map((item,index)=>{
+                        return(
+                            <tbody>
+                                <tr>
+                                <td key={index}>{index+1}</td>
+                                <td>{item.name}</td>
+                                <td>{item.email}</td>
+                                <td>@mdo</td>
+                                </tr>
+                            </tbody>
+                        )
+                    })}
+                </Table>
+            </Component>
         </Component>
     )
 }
